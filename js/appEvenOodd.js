835cad030d938eeed3842fd603f2af8d89f5d3da
AnatoliyZakhryapin <anatoliy.zakhryapin@gmail.com>
2023-10-13 09:35:31 +0200

    ADD numberRandomPc

--- js/appEvenOodd.js
+++ js/appEvenOodd.js
@@ -26,12 +26,35 @@ if (userChoice === "pari"){
 }
 console.log("isEvene", isEven);
 //     - Inserimento un numero da 1 a 5
-//         - Dichiarare la variabile "numberUser"
-//         - Assegnare il valore trammite prompt
+//         - Dichiarare la funzione "numberUserPrompt"
+function numberUserPrompt(x,y){
+    let text = "";
+    const min = x;
+    const max = y;
+    do {
+       text = prompt(`Inserisci un numero da ${min} a ${max}`)
+        // console.log(text, x, y)
+    }
+    while(text < x || text > y);
+    return text;
+}
+console.log(numberUserPrompt(1,5))
+//         - Assegnare il valore trammite function numberUserPrompt a variabile "numberChoice"
+const numberChoice = numberUserPrompt(1,5);
+console.log(numberChoice)
 //     - Generare un numero random 
 //         - Creare la variabile "min" con il valore minimo
+const min = 1;
 //         - Creare la variabile "max" con il valore massimo
-//         - Creare la funzione "numberRandomPc" che crea il numero random tra i valori min max   
+const max = 5;
+//         - Creare la funzione "numberRandomPc" che crea il numero random tra i valori min max 
+function numberRandomPc(min, max) {
+	min = Math.ceil(min)
+	max = Math.floor(max)
+	return Math.floor(Math.random() * (max - min + 1) + min)
+}  
+const numberPc = numberRandomPc(min,max);
+console.log("numberPc", numberPc)
 //     - Stabilire il vincitore 
 //         - Creamo la funzione "sum" e passiamo come i dati di ingresso il "numberUser" e il valore che ritorna da funzione "numberRandomPc"
 //         - Stabilire se dalla funzione "somma" ci torna il numero pari a dispari
